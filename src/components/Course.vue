ec3f334838713eb53be99ca46775aaca6f90da5e
Grebennikov Dmitriy <epitaphprince@gmail.com>
2019-04-23 22:34:35 +0300

    - add courses - add feedbacks - add image carousel component

---
+++ src/components/Course.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="container">
+        <img :alt="title" :src="getImgUrl(image)"/>
+        <p id="title">{{ title }}</p>
+        <p id="description">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String,
+            image: String,
+            description: String
+        },
+        methods : {
+            getImgUrl(imagePath) {
+                var images = require.context('../assets/main/courses', false, /\.jpg$/);
+                return images('./' + imagePath + ".jpg");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+ #container {
+     width: 370px;
+     height: 400px;
+     margin: 0 20px auto;
+ }
+    
+#container > img {
+    width: 370px;
+    height: 170px;
+}
+    
+#title {
+    font-size: 1.3rem;
+    font-weight: bold;
+}
+    
+#description {
+    line-height: 1.5;
+    text-align: left;
+    color: #312A1E;
+}
+    
+</style>
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,41 @@
+<template>
+    <Carousel :autoplay="true" 
+              :perPage="5" 
+              paginationActiveColor="#869791" 
+              paginationColor="#bbc3c0" 
+              :paginationSize="12" 
+              :paginationPadding="6">
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+        <Slide>Some slide</Slide>
+    </Carousel>
+</template>
+
+<script>
+    import { Carousel, Slide } from 'vue-carousel'
+
+    export default {
+        props: {
+            images: Array
+        },
+        components: {
+            Carousel,
+            Slide
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
